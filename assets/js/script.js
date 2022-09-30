254ce9b0b8034e0076545c1441499c3743e89257
sundayswellB356 <sundayswellB356@outlook.com>
2022-09-30 14:53:14 +0000

    Add eventListeners and DOM structure to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const userScore=0;
+const computerScore=0;
+
+const userScore_span=document.getElementById("user-score");
+const computerScore_span=document.getElementById("computer-score");
+
+const scoreBoard_div=document.querySelector(".scoreboard");
+
+const result_div=document.querySelector(".result");
+
+const rock_div=document.getElementById=("r");
+const paper_div=document.getElementById=("p");
+const scissors_div=document.getElementById=("s");
+
+rock_div.addEventListener("click".function()){
+    console.log("hey you clicked on rock");
+}
+paper_div.addEventListener("click".function()){
+    console.log("hey you clicked on paper");
+}

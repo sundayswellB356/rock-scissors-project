0aa59e8c6019f6e40fa65931ce1f8a834b6c7aa9
sundayswellB356 <sundayswellB356@outlook.com>
2022-09-30 18:16:21 +0000

    Add rock,paper,scissors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,22 @@
 const userScore=0;
 const computerScore=0;
-
+  
 const userScore_span=document.getElementById("user-score");
 const computerScore_span=document.getElementById("computer-score");
+const scoreboard_div=document.querySelector(".scoreboard");
 
-const scoreBoard_div=document.querySelector(".scoreboard");
+const result_div=document.querySelector(".result")
 
-const result_div=document.querySelector(".result");
+const rock_div=document.getElementById("r");
+const paper_div=document.getElementById("p");
+const scissors_div=document.getElementById("s");
 
-const rock_div=document.getElementById=("r");
-const paper_div=document.getElementById=("p");
-const scissors_div=document.getElementById=("s");
-
-rock_div.addEventListener('click',function(){
+rock_div .addEventListener('click', function(){
     console.log("hey you clicked on rock");
 })
-paper_div.addEventListener('click',function(){
+paper_div .addEventListener('click', function(){
     console.log("hey you clicked on paper");
 })
-scissors_div.addEventListener('click',function() {
-        console.log("hey you clicked on scissors");
-    })
+scissors_div .addEventListener('click', function(){
+    console.log("hey you clicked on scissors");
+})
